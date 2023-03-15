7c5a78318bcad1a9eb36b8142f46979bee430a09
isahua16 <isaelhuard@gmail.com>
2023-03-14 18:45:04 -0600

    cleaned up the logging so that it prints all variables on the same line

--- js/app.js
+++ js/app.js
@@ -3,7 +3,4 @@ let dogName = "Daisy";
 let userAge = 29;
 let dogAge = 2;
 
-console.log(userName);
-console.log(dogName);
-console.log(userAge);
-console.log(dogAge);
+console.log(userName, userAge, dogName, dogAge);
